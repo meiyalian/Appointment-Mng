b404ad53ee29bef18708f38a7de1a2e6ea5e7bf7
Alex Zhang <AlexZhang0914@gmail.com>
2021-05-07 12:20:22 +1000

    Frontend voucher frontend

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {DatabaseService} from "../database.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email = '';
+  password = '';
+  role = '';
+  aT= '';
+
+  constructor(private dbService: DatabaseService, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    let user={email:this.email, password:this.password};
+    this.dbService.loginUser(user).subscribe(result =>{
+      if (result["message"]=="ok"){
+        alert("success")
+      }
+      else {
+        alert("error")
+      }
+    });
+  }
+
+}
