8850dee9d6c87eb90fece08961e3807c3296a076
JIAYING WANG <jwan0085@student.monash.edu>
2021-05-21 13:17:19 +1000

    add node email

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -3,6 +3,8 @@ const User = require('../models/user');
 const BookingRequest = require('../models/bookingRequest');
 const Service = require('../models/service');
 let moment = require ('moment');
+let nodeemailer = require('nodemailer');
+const { getMaxListeners } = require('../models/user');
 module.exports = {
 
     //Customer can view their own voucher request list
@@ -10,16 +12,16 @@ module.exports = {
         User.findOne({_id:req.params.id})
         .populate('bookingRequest')
         .exec(function(err,user){
-            if(err) return res.status(400).json(err);
-            if(!user) return res.status(404).json();
-            res.json(user);
+            if(err) return res.status(400).json({ok:false,err:err});
+            if(!user) return res.status(404).json({ok:false,err:"user not found"});
+            res.json({ok:true,data:{bookingRequest:user.bookingRequest}});
         });
     },
 
     //Customer can add booking request 
     addBookingRequest: function(req,res){
-        User.findOne({_id:req.params.id},function(err,user){
-            newDate = Date(req.body.date)
+        User.findOne({_id:req.params.id}).exec (function(err,user){
+            newDate = new Date(req.body.date)
             var newbooking = new BookingRequest({
                 user: user._id,
                 date: newDate,
@@ -29,15 +31,40 @@ module.exports = {
             })
             user.bookingRequest.push(newbooking);
             newbooking.save(function(err){
-                if(err) return res.status(400).json(err);
+                if(err) return res.status(400).json({ok:false, err: err});
                 else{
-                    user.save(function(err){
-                        if(err) return res.status(400).json(err);
-                        else{
-                            return res.json(newbooking)
-                        }
-                    })
-                }
+
+                user.save(function(err){
+                    if(err) return res.status(400).json(err);
+                    else{
+
+                        Service.findOne({_id:newbooking.serviceType}).exec(function(err,serviceinfo){
+                            console.log(serviceinfo);
+                          let message = {
+                              from:"testemail90016@gmail.com",
+                              to: "user959.swen90016@gmail.com",
+                              subject:"Booking Request From Customer",
+                              html: `<h1>Booking Request for Voucher Booking System</h1><br>
+                              <h4>Customer name: ${user.name} </h4>
+                              <h4>Customer email: ${user.email} </h4>
+                              <h4>Customer phone number: ${user.phoneNumber} </h4>
+                              <h4>Service Type: ${serviceinfo.name} </h4>
+                              <h4>Service Delivery Options: ${newbooking.deliveryOption} </h4>
+                              <h4>Booking Date and time: ${newbooking.date} </h4>`
+                          }
+                          sendMail(message, info => {
+                              console.log(`The mail has beed send`);
+                            
+                            });
+                            return res.json({ok: true,
+                            data: newbooking,serviceinfo:serviceinfo})
+                        })
+                        
+      
+                    }
+                })
+              
+            }
             })
         })
     },
@@ -52,6 +79,31 @@ module.exports = {
             }).populate({path: 'bookingRequest', model: 'BookingRequest'}).exec(async (err, usr)=>{
                 if(err) return res.status(400).json(err);
                 else{
+                    BookingRequest.findOne({_id:req.params.bookingid}).exec(function(err,bookinginfo){
+                        console.log(bookinginfo)
+                        Service.findOne({_id:bookinginfo.serviceType}).exec(function(err,serviceinfo){
+                        console.log()
+                      let message = {
+                          from:"testemail90016@gmail.com",
+                          to: "user959.swen90016@gmail.com",
+                          subject:"Booking Cancel From Customer",
+                          html: `<h1>Booking Cancel for Voucher Booking System</h1><br>
+                          <h4>Customer name: ${usr.name} </h4>
+                          <h4>Customer email: ${usr.email} </h4>
+                          <h4>Customer phone number: ${usr.phoneNumber} </h4>
+                          <h4>Service Type: ${serviceinfo.name} </h4>
+                          <h4>Service Delivery Options: ${bookinginfo.deliveryOption} </h4>
+                          <h4>Booking Date and time: ${bookinginfo.date} </h4>`
+                      }
+                      sendMail(message, info => {
+                          console.log(`The mail has beed send`);
+                        
+                        });
+                    })
+                    })
+                    
+
+
                     await BookingRequest.findOneAndRemove({ _id: req.params.bookingid }, (err, booking)=>{
                              if(err) return res.status(400).json(err);
                     })
@@ -90,10 +142,49 @@ module.exports = {
     //adminAcceptBookingRequest and update the confirmation to be true
 
     adminAcceptBookingRequest: async function (req,res){
-      await BookingRequest.findOneAndUpdate({_id: req.params.id,confirmation:false},{$set:{confirmation:true}},{new:true},function(err,bookingRequest){
+      await BookingRequest.findOneAndUpdate({_id: req.params.id,confirmation:false},{$set:{confirmation:true}},{new:true}).
+      populate('user').
+      exec(function(err,bookingRequest){
             if(bookingRequest==null) return res.status(500).json({ok:false,err:"bad request"})
             if(err) return res.status(400).json({ok:false,err:err});
-            res.json({ok:true,data:{bookingRequest}});
+            else{
+
+                Service.findOne({_id:bookingRequest.serviceType}).exec(function(err,serviceinfo){
+                            console.log(serviceinfo);
+                          let message = {
+                              from:"testemail90016@gmail.com",
+                              to: bookingRequest.user.email,
+                              subject:"Booking Request Confirmation",
+                              html: `<h1>Your Booking Request for Voucher Booking System Has Been Confirmed</h1><br>
+                              <h4>Your name: ${bookingRequest.user.name} </h4>
+                              <h4>Your email: ${bookingRequest.user.email} </h4>
+                              <h4>Your phone number: ${bookingRequest.user.phoneNumber} </h4>
+                              <h4>Your Service Type: ${serviceinfo.name} </h4>
+                              <h4>Your Service Delivery Options: ${bookingRequest.deliveryOption} </h4>
+                              <h4>Your Booking Date and time: ${bookingRequest.date} </h4>`
+                          }
+                          sendMail(message, info => {
+                              console.log(`The mail has beed send`);
+                            
+                            });
+                            return res.json({ok: true,
+                            data: bookingRequest,serviceinfo:serviceinfo})
+                        })
+        }
         })
     }
+}
+
+function sendMail(message,callback){
+    let transporter = nodeemailer.createTransport({
+        host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "testemail90016@gmail.com",
+      pass: "catherine7join"
+    }
+});
+let info = transporter.sendMail(message);
+callback(info);
 }
